7d9d30ccae144ecf5b6dd41d9ee91563b22e2554
Bartosz Krawiec <krawiecbartosz55@gmail.com>
2023-06-01 23:27:26 +0200

    -

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 # grab the image from docker hub
 FROM mileschou/lapis:alpine
+
+# RUN luarocks install underscore.lua --from=http://marcusirven.s3.amazonaws.com/rocks/
+
 # make a project folder
 RUN mkdir /code
 WORKDIR /code
--- app.lua
+++ app.lua
@@ -1,9 +1,10 @@
-local lapis = require("lapis")
-local app = lapis.Application()
+-- app.lua
+local lapis = require "lapis"
+local json_params = require "lapis.application".json_params
 
-
+-- Define Category model
 local Category = {}
-Category.__index
+Category.__index = Category
 
 function Category.new(id, name)
   local self = setmetatable({}, Category)
@@ -12,10 +13,115 @@ function Category.new(id, name)
   return self
 end
 
-app:get("/", function()
-  local category = Category.new(1, "Test")
+local categories = {}
 
+-- Define Product model
+local Product = {}
+Product.__index = Product
+
+function Product.new(id, name, categoryId)
+  local self = setmetatable({}, Product)
+  self.id = id
+  self.name = name
+  self.categoryId = categoryId
+  return self
+end
+
+local products = {}
+
+-- Define Lapis application
+local app = lapis.Application()
+
+-- Category endpoints
+app:post("/categories", json_params({ name = "string" }), function(self)
+  local category = Category.new(#categories + 1, self.params.name)
+  categories[category.id] = category
   return { status = 201, json = { category } }
 end)
 
-return app
+app:get("/categories", function()
+  return { status = 200, json = categories }
+end)
+
+app:get("/categories/:id", function(self)
+  local category = categories[tonumber(self.params.id)]
+  if category then
+    return { status = 200, json = { category } }
+  else
+    return { status = 404, json = { message = "Category not found" } }
+  end
+end)
+
+app:put("/categories/:id", json_params({ name = "string" }), function(self)
+  local category = categories[tonumber(self.params.id)]
+  if category then
+    category.name = self.params.name
+    return { status = 200, json = { category } }
+  else
+    return { status = 404, json = { message = "Category not found" } }
+  end
+end)
+
+app:delete("/categories/:id", function(self)
+  local category = categories[tonumber(self.params.id)]
+  if category then
+    categories[tonumber(self.params.id)] = nil
+    return { status = 204 }
+  else
+    return { status = 404, json = { message = "Category not found" } }
+  end
+end)
+
+-- Product endpoints
+app:post("/products", json_params({ name = "string", categoryId = "number" }), function(self)
+  local category = categories[self.params.categoryId]
+  if category then
+    local product = Product.new(#products + 1, self.params.name, self.params.categoryId)
+    products[product.id] = product
+    return { status = 201, json = { product } }
+  else
+    return { status = 404, json = { message = "Category not found" } }
+  end
+end)
+
+app:get("/products", function()
+  return { status = 200, json = products }
+end)
+
+app:get("/products/:id", function(self)
+  local product = products[tonumber(self.params.id)]
+  if product then
+    return { status = 200, json = { product } }
+  else
+    return { status = 404, json = { message = "Product not found" } }
+  end
+end)
+
+app:put("/products/:id", json_params({ name = "string", categoryId = "number" }), function(self)
+  local product = products[tonumber(self.params.id)]
+  if product then
+    local category = categories[self.params.categoryId]
+    if category then
+      product.name = self.params.name
+      product.categoryId = self.params.categoryId
+      return { status = 200, json = { product } }
+    else
+      return { status = 404, json = { message = "Category not found" } }
+    end
+  else
+    return { status = 404, json = { message = "Product not found" } }
+  end
+end)
+
+app:delete("/products/:id", function(self)
+  local product = products[tonumber(self.params.id)]
+  if product then
+    products[tonumber(self.params.id)] = nil
+    return { status = 204 }
+  else
+    return { status = 404, json = { message = "Product not found" } }
+  end
+end)
+
+-- Start the Lapis server
+app:run()
