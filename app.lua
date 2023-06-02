e13ca641d707a4a7a3c9f748adb27be2bb0dec26
Bartosz Krawiec <krawiecbartosz55@gmail.com>
2023-06-02 19:47:35 +0200

    updated working version

--- app.lua
+++ app.lua
@@ -1,6 +1,6 @@
 -- app.lua
-local lapis = require "lapis"
-local json_params = require "lapis.application".json_params
+local lapis = require("lapis")
+local json_params = require("lapis.application").json_params
 
 -- Define Category model
 local Category = {}
@@ -15,29 +15,25 @@ end
 
 local categories = {}
 
--- Define Product model
-local Product = {}
-Product.__index = Product
+local newCategory = Category.new(#categories + 1, "A")
+categories[newCategory.id] = newCategory
 
-function Product.new(id, name, categoryId)
-  local self = setmetatable({}, Product)
-  self.id = id
-  self.name = name
-  self.categoryId = categoryId
-  return self
-end
+local newCategory = Category.new(#categories + 1, "B")
+categories[newCategory.id] = newCategory
 
-local products = {}
+local newCategory = Category.new(#categories + 1, "C")
+categories[newCategory.id] = newCategory
+
 
 -- Define Lapis application
 local app = lapis.Application()
 
 -- Category endpoints
-app:post("/categories", json_params({ name = "string" }), function(self)
+app:post("/categories", json_params(function(self)
   local category = Category.new(#categories + 1, self.params.name)
   categories[category.id] = category
   return { status = 201, json = { category } }
-end)
+end))
 
 app:get("/categories", function()
   return { status = 200, json = categories }
@@ -52,76 +48,8 @@ app:get("/categories/:id", function(self)
   end
 end)
 
-app:put("/categories/:id", json_params({ name = "string" }), function(self)
-  local category = categories[tonumber(self.params.id)]
-  if category then
-    category.name = self.params.name
-    return { status = 200, json = { category } }
-  else
-    return { status = 404, json = { message = "Category not found" } }
-  end
-end)
 
-app:delete("/categories/:id", function(self)
-  local category = categories[tonumber(self.params.id)]
-  if category then
-    categories[tonumber(self.params.id)] = nil
-    return { status = 204 }
-  else
-    return { status = 404, json = { message = "Category not found" } }
-  end
-end)
 
--- Product endpoints
-app:post("/products", json_params({ name = "string", categoryId = "number" }), function(self)
-  local category = categories[self.params.categoryId]
-  if category then
-    local product = Product.new(#products + 1, self.params.name, self.params.categoryId)
-    products[product.id] = product
-    return { status = 201, json = { product } }
-  else
-    return { status = 404, json = { message = "Category not found" } }
-  end
-end)
-
-app:get("/products", function()
-  return { status = 200, json = products }
-end)
-
-app:get("/products/:id", function(self)
-  local product = products[tonumber(self.params.id)]
-  if product then
-    return { status = 200, json = { product } }
-  else
-    return { status = 404, json = { message = "Product not found" } }
-  end
-end)
-
-app:put("/products/:id", json_params({ name = "string", categoryId = "number" }), function(self)
-  local product = products[tonumber(self.params.id)]
-  if product then
-    local category = categories[self.params.categoryId]
-    if category then
-      product.name = self.params.name
-      product.categoryId = self.params.categoryId
-      return { status = 200, json = { product } }
-    else
-      return { status = 404, json = { message = "Category not found" } }
-    end
-  else
-    return { status = 404, json = { message = "Product not found" } }
-  end
-end)
-
-app:delete("/products/:id", function(self)
-  local product = products[tonumber(self.params.id)]
-  if product then
-    products[tonumber(self.params.id)] = nil
-    return { status = 204 }
-  else
-    return { status = 404, json = { message = "Product not found" } }
-  end
-end)
 
 -- Start the Lapis server
-app:run()
+return app
